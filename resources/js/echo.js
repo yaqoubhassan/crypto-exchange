9dad911258b9c928dfe99c10d40d6d1dca858b73
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-10-11 17:31:17 +0100

    install reverb

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,17 +4,11 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-  broadcaster: 'pusher',
-  key: import.meta.env.VITE_PUSHER_APP_KEY,
-  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-  forceTLS: true,
-  encrypted: true,
-  authEndpoint: '/broadcasting/auth',
-  auth: {
-    headers: {
-      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content'),
-    },
-  },
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+  wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+  enabledTransports: ['ws', 'wss'],
 });
-
-export default window.Echo;
