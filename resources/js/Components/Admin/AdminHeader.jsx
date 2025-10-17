afdf7f1fb0d97fd6d053bbc91569a3716066d650
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-10-17 09:45:39 +0100

    minor fix

--- resources/js/Components/Admin/AdminHeader.jsx
+++ resources/js/Components/Admin/AdminHeader.jsx
@@ -331,13 +331,13 @@ export default function AdminHeader({ user, stats, selectedTimeframe, onTimefram
                                             <p className="text-sm font-medium text-gray-900">{user.name}</p>
                                             <p className="text-xs text-gray-500 mt-1">{user.email}</p>
                                         </div>
-                                        <Dropdown.Link href="/profile">
+                                        <Dropdown.Link href="/profile" className='flex items-center'>
                                             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                             </svg>
                                             Profile
                                         </Dropdown.Link>
-                                        <Dropdown.Link href="/security">
+                                        <Dropdown.Link href="/security" className='flex items-center'>
                                             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
                                             </svg>
